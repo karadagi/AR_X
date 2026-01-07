8c7d7e8564e5700a5580f4cfd59833d8e226d6cc
Ilker Karadag <decodeanddesign@gmail.com>
2026-01-06 19:52:31 -0500

    X

--- Antigravity/Sources/Models/STLLoader.swift
+++ Antigravity/Sources/Models/STLLoader.swift
@@ -31,64 +31,55 @@ class STLLoader {
         
         // 1. Extract Positions
         if let posAttr = mdlObject.vertexAttributeData(forAttributeNamed: MDLVertexAttributePosition, as: .float3) {
-             // posAttr.map.bytes is the raw pointer
-             // We need to copy this into a standard array [SIMD3<Float>]
-             // Accessing the buffer via map
-             let vertexCount = mdlObject.vertexCount
-             var positions = [SIMD3<Float>](repeating: .zero, count: vertexCount)
-             
-             positions.withUnsafeMutableBytes { ptr in
-                 // Copy from ModelIO buffer
-                 // Note: This assumes tightly packed float3. ModelIO layouts can vary.
-                 // A safer way is iterating, but slow. 
-                 // Allow stride check if possible, but for STL standard import usually it's packed.
-                 // Let's rely on standard layout for now or iterate.
-                 
-                 let bytesToCopy = vertexCount * MemoryLayout<SIMD3<Float>>.stride
-                 if posAttr.map.bytes.count >= bytesToCopy {
-                     ptr.copyMemory(from: UnsafeRawBufferPointer(start: posAttr.map.bytes, count: bytesToCopy))
-                 }
-             }
-             descriptor.positions = MeshBuffer(positions)
+            let vertexCount = mdlObject.vertexCount
+            let map = posAttr.map
+            let bytes = map.bytes.bindMemory(to: SIMD3<Float>.self, capacity: vertexCount)
+            
+            var positions = [SIMD3<Float>]()
+            positions.reserveCapacity(vertexCount)
+            
+            for i in 0..<vertexCount {
+                positions.append(bytes[i])
+            }
+            descriptor.positions = MeshBuffer(positions)
         }
         
         // 2. Extract Normals
         if let normAttr = mdlObject.vertexAttributeData(forAttributeNamed: MDLVertexAttributeNormal, as: .float3) {
-             let vertexCount = mdlObject.vertexCount
-             var normals = [SIMD3<Float>](repeating: .zero, count: vertexCount)
-             
-             normals.withUnsafeMutableBytes { ptr in
-                 let bytesToCopy = vertexCount * MemoryLayout<SIMD3<Float>>.stride
-                 if normAttr.map.bytes.count >= bytesToCopy {
-                     ptr.copyMemory(from: UnsafeRawBufferPointer(start: normAttr.map.bytes, count: bytesToCopy))
-                 }
-             }
-             descriptor.normals = MeshBuffer(normals)
+            let vertexCount = mdlObject.vertexCount
+            let map = normAttr.map
+            let bytes = map.bytes.bindMemory(to: SIMD3<Float>.self, capacity: vertexCount)
+            
+            var normals = [SIMD3<Float>]()
+            normals.reserveCapacity(vertexCount)
+            
+            for i in 0..<vertexCount {
+                normals.append(bytes[i])
+            }
+            descriptor.normals = MeshBuffer(normals)
         }
         
         // 3. Extract Indices (Primitives)
-        // flatten submeshes
-        var allIndices: [UInt32] = []
+        var allIndices = [UInt32]()
         if let submeshes = mdlObject.submeshes as? [MDLSubmesh] {
             for submesh in submeshes {
-                let indexCount = submesh.indexCount
                 let indexBuffer = submesh.indexBuffer
+                let indexCount = submesh.indexCount
                 let map = indexBuffer.map
-                
-                // ModelIO supports different index types (UInt8, UInt16, UInt32)
-                // We need to normalize to UInt32
                 let ptr = map.bytes
-                let type = submesh.indexType
                 
-                if type == .uInt32 {
-                     let indices = ptr.bindMemory(to: UInt32.self, capacity: indexCount)
-                     for i in 0..<indexCount { allIndices.append(indices[i]) }
-                } else if type == .uInt16 {
-                     let indices = ptr.bindMemory(to: UInt16.self, capacity: indexCount)
-                     for i in 0..<indexCount { allIndices.append(UInt32(indices[i])) }
-                } else if type == .uInt8 {
-                     let indices = ptr.bindMemory(to: UInt8.self, capacity: indexCount)
-                     for i in 0..<indexCount { allIndices.append(UInt32(indices[i])) }
+                switch submesh.indexType {
+                case .uInt32:
+                    let indices = ptr.bindMemory(to: UInt32.self, capacity: indexCount)
+                    for i in 0..<indexCount { allIndices.append(indices[i]) }
+                case .uInt16:
+                    let indices = ptr.bindMemory(to: UInt16.self, capacity: indexCount)
+                    for i in 0..<indexCount { allIndices.append(UInt32(indices[i])) }
+                case .uInt8:
+                    let indices = ptr.bindMemory(to: UInt8.self, capacity: indexCount)
+                    for i in 0..<indexCount { allIndices.append(UInt32(indices[i])) }
+                default:
+                    break
                 }
             }
         }
